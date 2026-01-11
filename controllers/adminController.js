7a7b222d83206f9b2f806e1e7b9fddd357ef30c5
Amardeep840690 <amar840690@gmail.com>
2026-01-11 21:59:30 +0530

    adding view count

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,10 +1,16 @@
 const User = require("../models/user");
+const View = require("../models/view");
 
-// Render admin page with all users
+// Render admin page with all users and visit count
 const renderAdminPanel = async (req, res) => {
   try {
     const users = await User.find().sort({ createdAt: -1 });
-    res.render("admin", { users });
+
+    // Get total visit count
+    let viewRecord = await View.findOne();
+    const totalViews = viewRecord ? viewRecord.totalViews : 0;
+
+    res.render("admin", { users, totalViews });
   } catch (error) {
     res.status(500).send("Failed to load admin panel");
   }
