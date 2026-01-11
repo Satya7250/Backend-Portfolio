aa34f826726b04a9bd6579c283bcc0b70ba01517
Amardeep840690 <amar840690@gmail.com>
2026-01-11 22:15:50 +0530

    adding view count

--- routes/view.js
+++ routes/view.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = express.Router();
 const { handleIncrementView } = require("../controllers/viewController");
 
+// POST route for incrementing view count
 router.post("/view", handleIncrementView);
+
+// GET route for testing (optional - can be removed later)
+router.get("/view", (req, res) => {
+  res.json({ message: "View endpoint is working. Use POST to increment count." });
+});
 
 module.exports = router;
